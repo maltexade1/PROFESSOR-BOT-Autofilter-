7822f87e4d0d8b331e12d748db18ea13190bb719
maltexade1 <40648156+maltexade1@users.noreply.github.com>
2024-09-30 09:02:01 +0100

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -101,7 +101,7 @@ async def pm_AutoFilter(client, msg, pmspoll=False):
         message = msg   
         if message.text.startswith("/"): return  # ignore commands
         if re.findall("((^\/|^,|^!|^\.|^[\U0001F600-\U000E007F]).*)", message.text): return
-        if 2 < len(message.text) < 100:
+        if 0 < len(message.text) < 100:
             search = message.text
             files, offset, total_results = await get_search_results(search.lower(), offset=0, filter=True)
             if not files: return await pm_spoll_choker(msg)              
